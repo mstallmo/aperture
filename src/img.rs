e7008eb478f7c8b9aa425221d6af3412e7c90d8e
Mason Stallmo <masonstallmo@gmail.com>
2019-01-12 19:56:41 -0600

    Re-order trait declaration

--- src/img.rs
+++ src/img.rs
@@ -17,6 +17,11 @@ impl GenericImage {
     }
 }
 
+pub trait DetectionImage {
+    fn dimension(&self) -> (u32, u32);
+    fn pixel_buffer(&self) -> Vec<u8>;
+}
+
 impl DetectionImage for GenericImage {
     fn dimension(&self) -> (u32, u32) {
         (self.width, self.height)
@@ -25,11 +30,6 @@ impl DetectionImage for GenericImage {
     fn pixel_buffer(&self) -> Vec<u8> {
         self.pixel_buffer.iter().cloned().collect()
     }
-}
-
-pub trait DetectionImage {
-    fn dimension(&self) -> (u32, u32);
-    fn pixel_buffer(&self) -> Vec<u8>;
 }
 
 impl DetectionImage for image::DynamicImage {
